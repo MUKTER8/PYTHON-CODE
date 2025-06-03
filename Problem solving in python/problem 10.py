ef429aa2ee8beaeb55990f9c98888ac43e488555
MUKTER HOSAIN <mokterprodhan8@gmail.com>
2025-06-03 11:02:20 +0600

    solve 10

---
+++ Problem solving in python/problem 10.py
@@ -0,0 +1,17 @@
+"""
+    Print the sum of both integers on the first line, 
+    the sum of both doubles (scaled to  decimal place) on the second line, 
+    and then the two concatenated strings on the third line.
+    """
+i = 4
+d = 4.0
+s = 'HackerRank '
+n1 = int(input())
+n2 = float(input())
+n3 = str(input())
+sum1 = i + n1
+sum2 = d + n2
+name = s + n3
+print(sum1)
+print(sum2)
+print(name)
