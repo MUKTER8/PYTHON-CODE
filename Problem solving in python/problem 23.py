af5d2434fd26a191afadb2a37e3381a5fbbb158f
MUKTER HOSAIN <mokterprodhan8@gmail.com>
2025-06-12 23:45:57 +0600

    solve 23

---
+++ Problem solving in python/problem 23.py
@@ -0,0 +1,16 @@
+str1 = input()
+str2 = input()
+str2 = str2 + "!"
+print("Hello", str1, str2, "You just delved into python.")
+
+"""
+#method 2:
+def print_full_name(first, last):
+    print(f"Hello {first} {last}! You just delved into python.")
+
+if __name__ == '__main__':
+    first_name = input()
+    last_name = input()
+    print_full_name(first_name, last_name)
+
+    """
